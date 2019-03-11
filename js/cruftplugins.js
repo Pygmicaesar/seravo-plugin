01d92f7777310d7122d9366d2bc76d8c008417d5
Susanna <lepolsussu@gmail.com>
2019-03-11 19:51:31 +0200

    Add delete-feature

--- js/cruftplugins.js
+++ js/cruftplugins.js
@@ -1,19 +1,17 @@
 'use strict';
 
-
-
-
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
   function seravo_ajax_delete_plugin(plugin_name, callback) {
     $.post(
-      seravo_cruftplugins_loc.ajaxurl,
-      { type: 'POST',
+      seravo_cruftplugins_loc.ajaxurl, {
+        type: 'POST',
         'action': 'seravo_remove_plugins',
         'removeplugin': plugin_name,
-        'nonce': seravo_cruftplugins_loc.ajax_nonce, },
-      function( rawData ) {
+        'nonce': seravo_cruftplugins_loc.ajax_nonce,
+      },
+      function (rawData) {
         var data = JSON.parse(rawData);
-        if ( data[ data.length - 1 ].indexOf('Success: Deleted 1 of 1 plugins.') != -1 ) {
+        if (data[data.length - 1].indexOf('Success: Deleted 1 of 1 plugins.') != -1) {
           callback();
         } else {
           confirm(seravo_cruftplugins_loc.failure);
@@ -24,11 +22,12 @@ jQuery(document).ready(function($) {
   // Generic ajax report loader function
   function seravo_plugin_load_report(section) {
     $.post(
-      seravo_cruftplugins_loc.ajaxurl,
-      { 'action': 'seravo_list_cruft_plugins',
+      seravo_cruftplugins_loc.ajaxurl, {
+        'action': 'seravo_list_cruft_plugins',
         'section': section,
-        'nonce': seravo_cruftplugins_loc.ajax_nonce, },
-      function(rawData) {
+        'nonce': seravo_cruftplugins_loc.ajax_nonce,
+      },
+      function (rawData) {
         if (rawData.length == 0) {
           $('#' + section).html(seravo_cruftplugins_loc.no_data);
         }
@@ -38,33 +37,59 @@ jQuery(document).ready(function($) {
         var data = JSON.parse(rawData);
         var sortedData = sort_plugin_data_array(data);
         // Show message
-        if ( sortedData.length != 0 ) {
-          $( '#cruftplugins_status' ).prepend('<p>' + seravo_cruftplugins_loc.cruftplugins + '</p>');
+        if (sortedData.length != 0) {
+          $('#cruftplugins_status').prepend('<p>' + seravo_cruftplugins_loc.cruftplugins + '</p>');
         } else {
-          $( '#cruftplugins_status' ).prepend('<b>' + seravo_cruftplugins_loc.no_cruftplugins + '</b>');
+          $('#cruftplugins_status').prepend('<b>' + seravo_cruftplugins_loc.no_cruftplugins + '</b>');
         }
         // Create tables
-        $.each(Object.keys(sortedData), function(i, status) {
+        $.each(Object.keys(sortedData), function (i, status) {
           // Create table body
           appendTable($bodyElement, status)
           // Insert entries
-          var $entries = $('#cruftplugins_' + status + ' .cruftplugins_entries');
+          var $entries = $('#cruftplugins_' + status + ' .cruftplugins_entries_' + status);
+          console.log($entries)
           $.each(sortedData[status], function (o, plugin) {
             var html = '<tr class="cruftplugin">' +
-              '<td class="cruftplugin-delete"><input data-plugin-name="' + plugin.name  + '" class="cruftplugin-check" type="checkbox"></td>' +
-              '<td class="cruftplugin-path">' + plugin.name  + '</td>' +
-            '</tr>';
-          $entries.append(html);
+              '<td class="cruftplugin-delete"><input data-plugin-name="' + plugin.name + '" class="cruftplugin-check_' + status + '" type="checkbox"></td>' +
+              '<td class="cruftplugin-path">' + plugin.name + '</td>' +
+              '</tr>';
+            $entries.append(html);
           })
-          if ( $('#cruftplugins_' + status).find('.cruftplugins_entries').children().length >= 30) {
-            $('#cruftplugins_' + status).find( '.cruftplugins_less-than' ).show(400);
+          if ($('#cruftplugins_' + status).find('.cruftplugins_entries_' + status).children().length >= 30) {
+            $('#cruftplugins_' + status).find('.cruftplugins_less-than').show(400);
           }
-        })
+          // Add checkbox click events
+          $('.cruftplugin-select-all_' + status).click(function (event) {
+            if (this.checked) {
+
+              // Iterate each checkbox
+              $('.cruftplugin-check_' + status + ', .cruftplugin-select-all_' + status).each(function () {
+                this.checked = true;
+              });
+            } else {
+              $('.cruftplugin-check_' + status + ', .cruftplugin-select-all_' + status).each(function () {
+                this.checked = false;
+              });
+            }
+          });
+          $('.cruftplugin-check_' + status).click(function () {
+            if ($('.cruftplugin-check_' + status + ':checked').length == $('.cruftplugin-check_' + status).length) {
+              $('.cruftplugin-select-all_' + status).each(function () {
+                this.checked = true;
+              });
+            } else {
+              $('.cruftplugin-select-all_' + status).each(function () {
+                this.checked = false;
+              });
+            }
+          })
+        });
         // Remove files
-        $('.cruftplugin-delete-button').click(function(event) {
+        $('.cruftplugin-delete-button').click(function (event) {
           event.preventDefault();
           var is_user_sure = confirm(seravo_cruftplugins_loc.confirm);
-          if ( ! is_user_sure ) {
+          if (!is_user_sure) {
             return;
           }
           var $this = $(this);
@@ -72,26 +97,27 @@ jQuery(document).ready(function($) {
           var $body = $('#cruftplugins_' + status);
           var cruft_list = [];
           var remove_rows = [];
+          console.log(status)
           // Get selected checkboxes
-          $body.find('.cruftplugin-check').each(function(){
+          $body.find('.cruftplugin-check_' + status).each(function () {
             var $cthis = $(this);
-            if ( $cthis.is(":checked") ) {
-              remove_rows.push( $cthis.parents(':eq(1)') );
-              cruft_list.push( $cthis.attr('data-plugin-name') );
+            if ($cthis.is(":checked")) {
+              remove_rows.push($cthis.parents(':eq(1)'));
+              cruft_list.push($cthis.attr('data-plugin-name'));
             }
           });
           // Delete plugins
-          if ( cruft_list.length > 0 ) {
-            seravo_ajax_delete_plugin(cruft_list, function() {
-              remove_rows.forEach(function( row ) {
+          if (cruft_list.length > 0) {
+            seravo_ajax_delete_plugin(cruft_list, function () {
+              remove_rows.forEach(function (row) {
                 row.animate({
                   opacity: 0
-                }, 600, function() {
+                }, 600, function () {
                   row.remove();
-                  if ( $body.find('.cruftplugins_entries').children().length >= 30 ) {
-                    $body.find( '.cruftplugins_less-than' ).show(400);
+                  if ($body.find('.cruftplugins_entries_' + status).children().length >= 30) {
+                    $body.find('.cruftplugins_less-than_' + status).show(400);
                   }
-                  if ( $body.find('.cruftplugins_entries').children().length == 0 ) {
+                  if ($body.find('.cruftplugins_entries_' + status).children().length == 0) {
                     $body.children().remove();
                     $body.append('<b>' + seravo_cruftplugins_loc.no_cruftplugins + '</b>');
                   }
@@ -101,11 +127,12 @@ jQuery(document).ready(function($) {
           }
         });
       }
-    ).fail(function() {
+    ).fail(function () {
       $('#' + section + '_loading').html(seravo_cruftplugins_loc.fail);
     });
   }
-  function sort_plugin_data_array (dataArray) {
+
+  function sort_plugin_data_array(dataArray) {
     var resultArray = {};
     $.each(dataArray, function (i, plugin) {
       // If no plugin key is created create one
@@ -118,28 +145,28 @@ jQuery(document).ready(function($) {
     return resultArray;
   }
 
-  function appendTable ($element, status) {
-    var html = 
-    '<b>' + seravo_cruftplugins_loc[status] + '</b><p>' + seravo_cruftplugins_loc[status + '_desc'] + '</p>'+
-    '<table id="cruftplugins_' + status + '">' +
+  function appendTable($element, status) {
+    var html =
+      '<b>' + seravo_cruftplugins_loc[status] + '</b><p>' + seravo_cruftplugins_loc[status + '_desc'] + '</p>' +
+      '<table id="cruftplugins_' + status + '">' +
       '<thead>' +
-        '<tr>' +
-          '<td><input class="cruftplugin-select-all" type="checkbox" data-enpassusermodified="yes"></td>' +
-          '<td class="cruft-tool-selector cruft-plugin-td"><b>Select all files</b></td>' +
-        '</tr>' +
+      '<tr>' +
+      '<td><input class="cruftplugin-select-all_' + status + '" type="checkbox"></td>' +
+      '<td class="cruft-tool-selector cruft-plugin-td"><b>Select all files</b></td>' +
+      '</tr>' +
       '</thead>' +
-      '<tbody class="cruftplugins_entries">' +
+      '<tbody class="cruftplugins_entries_' + status + '">' +
       '</tbody>' +
-      '<tfoot class="cruftplugins_less-than" style="display: none;">' +
-        '<tr>' +
-          '<td><input class="cruftplugin-select-all" type="checkbox"></td>' +
-          '<td class="cruft-tool-selector cruft-plugin-td"><b>Select all files</b></td>' +
-        '</tr>' +
+      '<tfoot class="cruftplugins_less-than_' + status + '" style="display: none;">' +
+      '<tr>' +
+      '<td><input class="cruftplugin-select-all_' + status + '" type="checkbox"></td>' +
+      '<td class="cruft-tool-selector cruft-plugin-td"><b>Select all files</b></td>' +
+      '</tr>' +
       '</tfoot>' +
-    '</table>' +
-    '<button class="cruftplugin-delete-button" data-status="' + status + '" type="button">Delete</button>' +
-    '</br>' +
-    '<hr class="plugin-separation-line">';
+      '</table>' +
+      '<button class="cruftplugin-delete-button" data-status="' + status + '" type="button">Delete</button>' +
+      '</br>' +
+      '<hr class="plugin-separation-line">';
     $element.append(html)
   }
 
