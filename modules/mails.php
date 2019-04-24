84b132d2b077ca50b41623540e42079afa3138f1
Samuli Lehtonen <samuli@seravo.fi>
2019-04-24 09:11:56 +0300

    WIP: Adjust add_submenu_page format

--- modules/mails.php
+++ modules/mails.php
@@ -31,7 +31,15 @@ if ( ! class_exists('Mails') ) {
     }
 
     public static function register_mails_page() {
-      add_submenu_page( 'tools.php', __('Mails', 'seravo'), __('Mails', 'seravo'), 'manage_options', 'mails_page', array( __CLASS__, 'load_mails_page' ), 'Seravo\seravo_postboxes_page' );
+      add_submenu_page(
+        'tools.php',
+        __('Mails', 'seravo'),
+        __('Mails', 'seravo'),
+        'manage_options',
+        'mails_page',
+        array( __CLASS__, 'load_mails_page' ),
+        'Seravo\seravo_postboxes_page'
+      );
     }
 
     public static function load_mails_page() {
